6aabf5a6539f329c01f17a0812dab0cec5d9384a
Tomas Smetana <tsmetana@redhat.com>
2013-06-21 16:22:47 +0200

    remove the hardcoded service url from the testing script

--- src/test.py
+++ src/test.py
@@ -9,14 +9,33 @@ def service_callback(h, srvurl, lifetime, errcode, data):
         print "Url: " + srvurl + ", timeout " + str(lifetime)
         rv = True
     elif errcode == 1:
-        print "No more results"
+        print "Services: No more results"
     else:
         print "Error: " + str(errcode)
 
     return rv
 
+def attr_callback(h, attrlist, errcode, data):
+    rv = False
+    if errcode == 0:
+        print "Attrs: " + attrlist
+        rv = True
+    elif errcode == 1:
+        print "Attrs: No more results"
+    else:
+        print "Error: " + str(errcode)
+
+    return rv
+
+if len(sys.argv) < 2:
+    sys.exit('Usage: %s <service url>' % sys.argv[0])
+
+url = sys.argv[1];
+
 hslp = slp.SLPOpen("en", False)
-err = slp.SLPFindSrvs(hslp, "service:ssh.openslp", None, None, service_callback, None);
+
+slp.SLPFindSrvs(hslp, url, None, None, service_callback, None);
+slp.SLPFindAttrs(hslp, url, None, None, attr_callback, None);
 
 slp.SLPClose(hslp);
 
